52c54a29ba6c7b28d7872cffd89f1bbb179e8182
Rafael Ronsoni Gaidzinski <rafael.513011@alunosatc.edu.br>
2025-09-17 19:43:44 -0300

    Feito o design pattern singleton e factory method

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,50 @@
-console.log("Hello, World! 001");
+// Classes concretas
+class Bicicleta {
+    move() {
+      return "Pedalando a bicicleta no pátio da UNISATC";
+    }
+  }
+  
+class Patinete {
+    move() {
+        return "Andando de patinete pelo estacionamento da UNISATC";
+    }
+}
+
+class Onibus {
+    move() {
+        return "Andando de ônibus e chegando na UNISATC";
+    }
+}
+
+// Factory Method
+class TransportFactory {
+    static types = {
+        bicicleta: Bicicleta,
+        patinete: Patinete,
+        onibus: Onibus
+    }; 
+
+    static createTransport(type) {
+        const TransportClass = this.types[type];
+        if (!TransportClass) {
+            throw new Error("Tipo de transporte ainda não suportado");
+          }
+        return new TransportClass();
+    }
+}
+
+function main() {
+    const transports = ["bicicleta", "patinete", "onibus"];
+    try {
+        transports.forEach((type) => {
+            const transport = TransportFactory.createTransport(type);
+            let response = transport.move();
+            console.log(response);
+      });
+    } catch (error) {
+      console.error("Erro ao criar transporte ...", error.message);
+    }
+  }
+  
+  main();
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,36 @@
-console.log("Hello, World! 002");
+class Config {
+    constructor() {
+        if (Config.instance) {
+        return Config.instance;
+        }  
+
+        this.settings = {};
+        Config.instance = this;
+    }
+
+    setConfig(key, value) {
+        this.settings[key] = value;
+    }
+
+    getConfig(key) {
+        return this.settings[key];
+    }
+
+    getAllConfig() {
+        Object.entries(this.settings).forEach(([key, value])=> {
+            console.log(key + ": " + value)
+        });
+        console.log();
+    }
+}
+  
+const c1 = new Config();
+c1.setConfig("firstLang", "pt-BR");
+console.log(c1.getConfig("firstLang"));
+c1.getAllConfig();
+
+
+const c2 = new Config();
+c2.setConfig("secondLang", "en-US");
+console.log(c2.getConfig("secondLang"));
+c2.getAllConfig();
